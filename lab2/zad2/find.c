cb6a25bad3bb9f4717b7e91bcb5c7c53def9c6e4
surjak <surjak1999@gmail.com>
2020-03-12 21:10:09 +0100

    workssssssssss

--- lab2/zad2/find.c
+++ lab2/zad2/find.c
@@ -165,6 +165,83 @@ void mtime(char *root_path, char mode, int count, time_t date)
     closedir(dir);
 }
 
+void atime(char *root_path, char mode, int count, time_t date)
+{
+    // if (count == 0)
+    //     return;
+    if (root_path == NULL)
+        return;
+    DIR *dir = opendir(root_path);
+
+    if (dir == NULL)
+    {
+        fprintf(stderr, "error open dir: %s\n", strerror(errno));
+        exit(-1);
+    }
+
+    struct dirent *file;
+
+    char new_path[256];
+    while ((file = readdir(dir)) != NULL)
+    {
+        strcpy(new_path, root_path);
+        strcat(new_path, "/");
+        strcat(new_path, file->d_name);
+
+        struct stat sb;
+
+        if (lstat(new_path, &sb) < 0)
+        {
+            fprintf(stderr, "unable to lstat file %s: %s\n", new_path, strerror(errno));
+            exit(-1);
+        }
+
+        if (S_ISDIR(sb.st_mode))
+        {
+            if (strcmp(file->d_name, ".") == 0 || strcmp(file->d_name, "..") == 0)
+            {
+                continue;
+            }
+
+            atime(new_path, mode, count, date);
+        }
+        time_t modif_time = sb.st_atime;
+        if (mode == '-')
+        {
+            int diff_modif = difftime(date, modif_time);
+            if (!((diff_modif == 0 && mode == '=') || (diff_modif > 0 && mode == '+') || (diff_modif < 0 && mode == '-')))
+                continue;
+
+            print_from_stat(new_path, &sb);
+            // printf("%d\n\n", diff_modif);
+        }
+        else if (mode == '+')
+        {
+            int diff_modif = difftime(date, modif_time);
+            if (!((diff_modif == 0 && mode == '=') || (diff_modif > 0 && mode == '+') || (diff_modif < 0 && mode == '-')))
+                continue;
+
+            print_from_stat(new_path, &sb);
+            // printf("%d\n\n", diff_modif);
+        }
+        else if (mode == '=')
+        {
+
+            int diff_modif = abs(difftime(modif_time, date)) / (60 * 60 * 24);
+            int diff_modif2 = difftime(date, modif_time);
+
+            // int diff_modif_2 = difftime(modif_time, date);
+
+            // printf("%d\n\n", diff_modif);
+            if ((diff_modif == 0 && mode == '=') && !(diff_modif2 < 0 && mode == '='))
+                print_from_stat(new_path, &sb);
+
+            // printf("%d\n\n", diff_modif);
+        }
+    }
+    closedir(dir);
+}
+
 void print_files_handler(char *root_path, char *command, char mode, int count)
 {
     if (strcmp(command, "maxdepth") == 0)
@@ -183,11 +260,23 @@ void print_files_handler(char *root_path, char *command, char mode, int count)
         // print_files(argv[1], argv[2][0], mktime(timeinfo), print_from_stat);
         mtime(root_path, mode, count, mktime(timeinfo));
     }
+    else if (strcmp(command, "atime") == 0)
+    {
+        time_t rawtime;
+        struct tm *timeinfo;
+
+        time(&rawtime);
+        timeinfo = localtime(&rawtime);
+        timeinfo->tm_mday -= count;
+        // printf("Current local time and date: %s", asctime(timeinfo));
+        // print_files(argv[1], argv[2][0], mktime(timeinfo), print_from_stat);
+        atime(root_path, mode, count, mktime(timeinfo));
+    }
 }
 
 int main()
 {
     char tab[5] = {'a'};
     // print_files_handler(".", "maxdepth", tab[0], 6);
-    print_files_handler("../../", "mtime", '=', 3);
+    print_files_handler("../../", "atime", '-', 2);
 }
