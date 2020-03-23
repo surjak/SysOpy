763bb74862fb41a8df3a18212f0bbc79f258aba1
surjak <surjak1999@gmail.com>
2020-03-23 19:51:25 +0100

    ehhhh 2

--- lab3/zad1/main.c
+++ lab3/zad1/main.c
@@ -1,100 +1,51 @@
-#include <dirent.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <dirent.h>
 #include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <time.h>
 #include <unistd.h>
-#include <errno.h>
+#include <wait.h>
 
-const char format[] = "%Y-%m-%d %H:%M:%S";
-
-void follow_dir()
+void search_dir(char *path)
 {
-    DIR *root_dir = opendir(".");
-    if (root_dir == NULL)
+    if (!path)
+        return;
+    DIR *directory = opendir(path);
+    if (!directory)
+        return;
+    struct dirent *d;
+    struct stat st;
+
+    while ((d = readdir(directory)))
     {
-        fprintf(stderr, "Cant open directory, errno: %s\n", strerror(errno));
-        exit(-1);
-    }
-
-    struct dirent *file;
-
-    while ((file = readdir(root_dir)) != NULL)
-    {
-        struct stat sb;
-        if (lstat(file->d_name, &sb) < 0)
+        if (strcmp(d->d_name, "..") == 0)
+            continue;
+        if (strcmp(d->d_name, ".") == 0)
+            continue;
+        char *dir = malloc(sizeof(char) * (strlen(path) + strlen(d->d_name)) + 2);
+        sprintf(dir, "%s/%s", path, d->d_name);
+        if (lstat(dir, &st) < 0)
+            continue;
+        if (S_ISDIR(st.st_mode) && fork() == 0)
         {
-            fprintf(stderr, "can't lstat file %s, errno: %s\n", file->d_name, strerror(errno));
-            exit(-1);
+            printf("\npid(%i) path(%s)\n", getpid(), dir);
+            static const char cmd[] = "ls -la %s";
+            char *ls = calloc(sizeof(cmd), sizeof(char));
+            sprintf(ls, cmd, dir);
+            system(ls);
+            free(ls);
+            exit(0);
         }
-        if (S_ISDIR(sb.st_mode))
-        {
-            if (strcmp(file->d_name, ".") == 0 || strcmp(file->d_name, "..") == 0)
-            {
-                continue;
-            }
-            if (chdir(file->d_name) != 0)
-            {
-                fprintf(stderr, "unable to change path to %s", file->d_name);
-                exit(-1);
-            }
-
-            follow_dir();
-            pid_t pid_fork = fork();
-            if (pid_fork < 0)
-            {
-                fprintf(stderr, "cant fork, errno: %s", strerror(errno));
-                exit(-1);
-            }
-            else if (pid_fork == 0)
-            {
-                char cwd[4096];
-                if (getcwd(cwd, 4096) == NULL)
-                {
-                    fprintf(stderr, "unable to get current working directory, errno: %s", strerror(errno));
-                    exit(-1);
-                }
-                printf("dir: %s, PID: %d\n", cwd, getpid());
-                int exec_status = execlp("ls", "ls", "-l", NULL);
-                if (exec_status != 0)
-                {
-                    fprintf(stderr, "exec failed, errno: %s\n", strerror(errno));
-                    exit(-1);
-                }
-                exit(exec_status);
-            }
-            else
-            {
-                wait(0);
-            }
-            if (chdir("..") != 0)
-            {
-                fprintf(stderr, "unable to change path to %s/../, errno: %s", file->d_name, strerror(errno));
-                exit(-1);
-            }
-        }
-        // printf("%s\n", file->d_name);
+        else
+            wait(NULL);
+        free(dir);
     }
-    closedir(root_dir);
+    closedir(directory);
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-    if (argc != 2)
-    {
-        fprintf(stderr, "wrong arguments\n");
-        exit(-1);
-    }
-
-    if (chdir(argv[1]) != 0)
-    {
-        fprintf(stderr, "cant chmod to %s\n", argv[1]);
-        exit(-1);
-    }
-
-    follow_dir();
+    char *path = argc > 1 ? argv[1] : ".";
+    search_dir(path);
     return 0;
 }
