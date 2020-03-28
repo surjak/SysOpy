302539daa412b3e00b9b1be2ac4ff608ba5a86ea
surjak <surjak1999@gmail.com>
2020-03-28 11:54:53 +0100

    zad3

---
+++ lab4/zad3/main.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+
+void handler_CHILD(int sig, siginfo_t *info, void *ucontext)
+{
+    printf("Signal number %d\n", info->si_signo);
+    printf("Sending process ID %d\n", info->si_pid);
+    printf("Child exit value %d\n", info->si_status);
+}
+
+void handle_SIGINT(int sig, siginfo_t *info, void *ucontext)
+{
+    printf("Signal number %d\n", info->si_signo);
+    printf("Sending process ID %d\n", info->si_pid);
+    if (info->si_code == SI_USER)
+    {
+        printf("USER\n");
+    }
+    else if (info->si_code == SI_KERNEL)
+    {
+        printf("KERNEL\n");
+    }
+    else if (info->si_code == SI_QUEUE)
+    {
+        printf("SIGQUEUE\n");
+    }
+}
+void handle_QUEUE(int sig, siginfo_t *info, void *ucontext)
+{
+    printf("Signal number %d\n", info->si_signo);
+    printf("Sending process ID %d\n", info->si_pid);
+    printf("Value %d\n", info->si_value.sival_int);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        printf("./main [child/status/queue]");
+        exit(-1);
+    }
+
+    struct sigaction act;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = SA_SIGINFO;
+    if (strcmp(argv[1], "child") == 0)
+    {
+        act.sa_sigaction = handler_CHILD;
+        sigaction(SIGCHLD, &act, NULL);
+        pid_t child_pid = fork();
+
+        if (child_pid == 0)
+        {
+            exit(123);
+        }
+
+        wait(NULL);
+    }
+    else if (strcmp(argv[1], "queue") == 0)
+    {
+        act.sa_sigaction = handle_QUEUE;
+        sigaction(SIGINT, &act, NULL);
+
+        union sigval sigval;
+        sigval.sival_int = 123;
+
+        sigqueue(getpid(), SIGINT, sigval);
+    }
+    else if (strcmp(argv[1], "sigint") == 0)
+    {
+
+        act.sa_sigaction = handle_SIGINT;
+        sigaction(SIGINT, &act, NULL);
+        union sigval sigval;
+
+        sigqueue(getpid(), SIGINT, sigval);
+    }
+
+    return 0;
+}
