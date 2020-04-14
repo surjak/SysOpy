63f19a72ff0eccd7cbce02aafa7689e0816b8af1
surjak <surjak1999@gmail.com>
2020-04-14 14:45:21 +0200

    handle init from server side

--- lab6/zad1/server.c
+++ lab6/zad1/server.c
@@ -40,6 +40,24 @@ void send_private(int client_id, message *message)
     }
 }
 
+int find_client_id()
+{
+    if (clients_tab_size < MAX_CLIENTS)
+    {
+        num_clients++;
+        return clients_tab_size++;
+    }
+    for (int i = 0; i < MAX_CLIENTS; i++)
+    {
+        if (clients[i] == -1)
+        {
+            num_clients++;
+            return i;
+        }
+    }
+    return -1;
+}
+
 void handle_sigint(int sig)
 {
     printf("Server received SIGINT\n");
@@ -56,6 +74,29 @@ void handle_sigint(int sig)
         {
             send_private(i, &message);
         }
+    }
+}
+
+void handle_init(message *mess)
+{
+    key_t key;
+    scanf(mess->text, "%d", &key);
+    int client_id = find_client_id();
+    if (client_id != -1)
+    {
+        if ((clients[client_id] = get_queue(key)) == -1)
+        {
+            perror("can't open client private queue");
+        }
+        message message;
+        message.type = TYPE_INIT;
+        message.id = client_id;
+        send_private(client_id, &message);
+        printf("sent register confirmation for client: %d\n", client_id);
+    }
+    else
+    {
+        fprintf(stderr, "clients full error\n");
     }
 }
 
@@ -76,8 +117,37 @@ int main()
     sa.sa_flags = 0;
     sa.sa_handler = handle_sigint;
     sigaction(SIGINT, &sa, NULL);
+    key_t key = get_public_key();
+    if ((queue = create_queue(key)) == -1)
+    {
+        perror("cant create queue");
+        exit(1);
+    }
+
+    message message;
     while (1)
     {
+        if (receive(queue, &message) == -1)
+        {
+            printf("Can't receive \n");
+            if (errno != EINTR)
+            {
+                perror("can't receive message");
+                exit(1);
+            }
+            continue;
+        }
+        printf("alalalal\n");
+        switch (message.type)
+        {
+        case TYPE_INIT:
+            handle_init(&message);
+            break;
+
+        default:
+            fprintf(stderr, "wrong type\n");
+            break;
+        }
     }
     return 0;
 }
