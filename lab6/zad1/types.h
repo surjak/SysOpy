fb18d6bcf3a2fee5542f3d7ab85cf541103fbba0
surjak <surjak1999@gmail.com>
2020-04-14 16:08:03 +0200

    deleted unused types

--- lab6/zad1/types.h
+++ lab6/zad1/types.h
@@ -2,14 +2,6 @@
 #define TYPES_H
 
 #define TYPE_STOP 1
-#define TYPE_LIST 2
-#define TYPE_FRIENDS 3
-#define TYPE_ECHO 4
-#define TYPE_2ALL 5
-#define TYPE_2FRIENDS 6
-#define TYPE_2ONE 7
-#define TYPE_ADD 8
-#define TYPE_DEL 9
 #define TYPE_INIT 10
 
 #define TYPE_LAST 10
